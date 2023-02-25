9dd75c6c7f7757f2ae3b58c4452a8fa390cc7a7a
Ana Salvação <ana.cvg.antunes@gmail.com>
2023-02-25 19:38:10 +0000

    "25-02-2023"

--- web/2 - Validations Fetch and Register/server/app.py
+++ web/2 - Validations Fetch and Register/server/app.py
@@ -68,38 +68,20 @@ async def register(player: sch.PlayerRegister) -> sch.PlayerRegisterResult:
 
 ################################################################################
 
-
-
-
-
-
-
-
-
-
-
-################################################################################
-
-
 def main():
     import uvicorn
     from docopt import docopt
     help_doc = """
 A Web accessible FastAPI server that allow players to register/enroll
 for tournaments.
-
 Usage:
   app.py [-c | -c -d] [-p PORT] [-h HOST_IP]
-  
-  
 Options:
   -p PORT, --port=PORT          Listen on this port [default: 8000]
   -c, --create-ddl              Crea    te datamodel in the database
   -d, --populate-db             Populate the DB with dummy for testing purposes
   -h HOST_IP, --host=HOST_IP    Listen on this IP address [default: 127.0.0.1]
-
 """
-
     args = docopt(help_doc)
     create_ddl = args['--create-ddl']
     populate_db = args['--populate-db']
@@ -109,7 +91,7 @@ Options:
             print("Will also populate the DB")
         #:
     #:
-    
+
     uvicorn.run(
         'app:app',
         port = int(args['--port']), 
@@ -117,12 +99,7 @@ Options:
         reload = True,
     )
 #:
-    #uvicorn.run('app:app', reload=True)
-#
 
 if __name__ == '__main__':
     main()
 #:
-    
-    
-    
